7e43a996f8fe348bd61551cd5a6ec4e9562eaed0
Shufeng Tan <shufengtan@gmail.com>
2026-02-11 19:49:44 -0500

    Polished fidelity_positions methods

--- fidelity_positions.py
+++ fidelity_positions.py
@@ -8,21 +8,32 @@ from plotly.subplots import make_subplots
 class FidelityPositions:
     csv_file: str
     df_pos: pd.DataFrame = field(init=False)
+    total_cash: float = 0.0
+    total_value: float = 0.0
     def __post_init__(self):
         self.df_pos = self.load_fidelity_positions(self.csv_file)
 
     def load_fidelity_positions(self, csv_file):
+        df_pos = self.read_fidelity_porfolio_position_csv_file(csv_file)
+        df_pos = df_pos[~df_pos['Average Cost Basis'].isna() & (df_pos['Last Price'] != '--')]
+        numeric_cols = ['Quantity', 'Cost Basis Total', 'Average Cost Basis', 'Last Price']
+        for col in numeric_cols:
+            df_pos[col] = df_pos[col].apply(lambda x: x.replace('$', '') if type(x) is str else x)
+            df_pos[col] = pd.to_numeric(df_pos[col], errors='coerce')
+        return df_pos
+
+    def read_fidelity_porfolio_position_csv_file(self, csv_file):
         df_pos = pd.read_csv(csv_file, index_col=None)
         if np.any(df_pos.index.str.contains('X')):
             preserved_cols = df_pos.columns
             df_pos = df_pos.reset_index().iloc[:, :-1]
             df_pos.columns = preserved_cols
         df_pos = df_pos.drop(columns=[c for c in df_pos.columns if 'Gain/Loss' in c])
-        df_pos = df_pos[~df_pos['Average Cost Basis'].isna() & (df_pos['Last Price'] != '--')]
-        numeric_cols = ['Quantity', 'Cost Basis Total', 'Average Cost Basis', 'Last Price']
-        for col in numeric_cols:
-            df_pos[col] = df_pos[col].apply(lambda x: x.replace('$', '') if type(x) is str else x)
-            df_pos[col] = pd.to_numeric(df_pos[col], errors='coerce')
+        df_pos = df_pos[~pd.isna(df_pos['Current Value']) & (df_pos['Current Value'] != '--')]
+        df_pos['Current Value'] = pd.to_numeric(df_pos['Current Value'].apply(lambda x: x.replace('$', '')))
+        df_cash = df_pos[df_pos['Last Price'].isna() & df_pos['Symbol'].str.contains(r'Pending activity|USD\*\*\*|XX\*\*')]
+        self.total_cash = df_cash['Current Value'].sum()
+        self.total_value = df_pos['Current Value'].sum()
         return df_pos
 
     def get_option_positions(self):
@@ -46,7 +57,7 @@ class FidelityPositions:
         })
         _df = _df.groupby(['strategy', 'symbol']).sum().reset_index()
         risk_dict = dict(_df.groupby('strategy').amount.sum())
-        strat_list = list(_df.strategy.unique())
+        strat_list = sorted(_df.strategy.unique(), reverse=True)
         titles = [f'{s} option total: {risk_dict[s].item()}' for s in strat_list]
         fig = make_subplots(rows=1, cols=len(titles), subplot_titles=titles, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
         for _j, strategy in enumerate(strat_list):
